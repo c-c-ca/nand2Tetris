9d82a33c9372c490eca2f1bea9b35723b70a0f88
Adam Carter <adm.carter@gmail.com>
2020-05-31 23:51:02 -0400

    initial commit

---
+++ projects/04/fill/Fill.asm
@@ -0,0 +1,61 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/04/Fill.asm
+
+// Runs an infinite loop that listens to the keyboard input.
+// When a key is pressed (any key), the program blackens the screen,
+// i.e. writes "black" in every pixel;
+// the screen should remain fully black as long as the key is pressed. 
+// When no key is pressed, the program clears the screen, i.e. writes
+// "white" in every pixel;
+// the screen should remain fully clear as long as no key is pressed.
+
+// Screen clearing code:
+// Initialize pointer to clear screen
+(BEGIN)
+    @SCREEN
+    D=A      // D=SCREEN
+    @ptr
+    M=D      // ptr=SCREEN
+(CLEAR)
+    @ptr
+    A=M
+    M=0      // *ptr=0
+    @ptr
+    M=M+1    // ptr++
+    D=M
+    @KBD
+    D=D-A
+    @CLEAR
+    D;JLT    // If ptr<KBD goto CLEAR
+(KEY_DOWN)   // loops until a key is pressed
+    @KBD
+    D=M
+    @KEY_DOWN
+    D;JEQ
+
+// Screen filling code:
+// Initialize pointer to fill screen
+    @SCREEN
+    D=A      // D=SCREEN
+    @ptr
+    M=D      // ptr=SCREEN
+(FILL)
+    @ptr
+    A=M
+    M=-1     // *ptr=0
+    @ptr
+    M=M+1    // ptr++
+    D=M
+    @KBD
+    D=D-A
+    @FILL
+    D;JLT    // If ptr<KBD goto FILL
+(KEY_UP)
+    @KBD
+    D=M
+    @KEY_UP
+    D;JNE
+    @BEGIN
+    0;JMP
